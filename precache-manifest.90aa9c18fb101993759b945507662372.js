bd17e7e4f188b2fa3f55b994753fee187011da9a
munzer9 <72735899+munzer9@users.noreply.github.com>
2020-12-16 00:01:05 +0100

    hi

---
+++ precache-manifest.90aa9c18fb101993759b945507662372.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7e2bd649723f049d2b08f0375e7f2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db87d1ae09bd69ccd5b",
+    "url": "/static/js/2.80ac774c.chunk.js"
+  },
+  {
+    "revision": "882c6d5fa5804c51c177",
+    "url": "/static/js/main.32a7d30e.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
